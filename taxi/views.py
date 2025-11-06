331671c196bf3f219182db357cf56834bbadbed4
Vasyl Cherkes <cherkesva@gmail.com>
2025-11-06 20:23:39 +0200

    Solution

--- taxi/views.py
+++ taxi/views.py
@@ -25,7 +25,7 @@ class ManufacturerListView(generic.ListView):
 
 class CarListView(generic.ListView):
     model = Car
-    queryset = Car.objects.select_related("manufacturer").order_by("model")
+    queryset = Car.objects.select_related("manufacturer").order_by("id")
 
     paginate_by = 5
 
@@ -43,4 +43,4 @@ class DriverListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.all()
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
